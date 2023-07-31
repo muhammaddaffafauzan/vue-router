2f432ad5846c933970222558b2d14a3897f8a420
muhammaddaffafauzan <bayu_217@smkassalaambandung.sch.id>
2023-07-31 12:01:13 +0700

    router coy

---
+++ src/views/Produk.vue
@@ -0,0 +1,50 @@
+<template>
+  <h1>Produk</h1>
+  <div class="flex-container">
+   <div v-for="produk in state" :key="produk.id" class="card">
+    <router-link class="container" :to="{ name: 'Detail', params: { id_produk: produk.id }}">
+    <h4>{{ produk.nama }}</h4>
+    </router-link>
+  </div>
+  </div>
+</template>
+<script>
+import { onMounted, reactive } from 'vue'
+import { produk } from '@/assets/produk.js'
+export default{
+  setup (props, context)
+{
+  const state = reactive(produk["produk"]);
+
+onMounted(() => {
+      context.emit("id-menu", 1);
+  });
+  return{
+    state
+  }
+ }
+}
+</script >
+<style scoped>
+.flex-container {
+ display: flex;
+}
+ .card {
+ /* Add shadows to create the "card" effect */
+ box-shadow: 0 4px 8px rgba(0,0,0,0.2);
+ transition: 0.35;
+ margin: 10px;
+ min-width: 200px;
+ cursor: pointer;
+ }
+
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+ box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+/* Add some padding inside the card container */
+ .container {
+  padding: 2px 16px;
+}
+
+</style>
